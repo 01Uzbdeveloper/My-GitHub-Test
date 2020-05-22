103f0a84c156fcd78fe171814757f3ce317566e8
01Uzbdeveloper <64874244+01Uzbdeveloper@users.noreply.github.com>
2020-05-22 16:01:47 +0500

    Create header.php

---
+++ header.php
@@ -0,0 +1,57 @@
+<html>
+    <head><title>Quiz Box website</title>
+      <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      <link rel="stylesheet" type="text/css" href="main.css" />
+       <link href='http://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic' rel='stylesheet' type='text/css'> 
+     <style>
+        body{
+        background-color:red;     
+        background:url('images/test_1/image_7.jpg');
+        background-size:1300px 500px;
+        background-repeat: no-repeat;
+        h{
+        color:white;    
+        }
+        }
+       ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #333333;
+            }
+            li {
+            float: left;    
+            }
+            li a {
+            display: block;
+            color:white;
+            text-align: center;
+            padding: 16px;
+            text-decoration: none;
+            }
+            li a:hover {
+            background-color:#111111;
+            }
+
+    </style>
+    </head>
+    <body> 
+        
+        
+        <ul>
+               <li> <nav id="mySidenav" class="w3-sidebar w3-collapse w3-white w3-card-2">       
+        <a class="w3-bar-item w3-button active" title="Home" ><mark>Quiz Box</mark></a></li>           
+               <li> <nav id="mySidenav" class="w3-sidebar w3-collapse w3-white w3-card-2">
+        <a class="w3-bar-item w3-button active" title="Home" href="index.php?p=index">Home</a></li>
+              <li><nav id="mySidenav" class="w3-sidebar w3-collapse w3-white w3-card-2">
+        <a class="w3-bar-item w3-button active" title="Subjects" href="index.php?p=subjects">Subjects</a></li>
+              <li>  <nav id="mySidenav" class="w3-sidebar w3-collapse w3-white w3-card-2">
+        <a class="w3-bar-item w3-button active" title="Contact" href="index.php?p=contact">Contact</a></li>
+              <li><nav id="mySidenav" class="w3-sidebar w3-collapse w3-white w3-card-2">      
+        <a class="w3-bar-item w3-button active" title="About" href="index.php?p=about">About</a></li>
+         
+        </ul>
+      
+
+ 
