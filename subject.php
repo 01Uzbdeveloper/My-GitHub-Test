b8427da593b7d62aef1b68811e6ec78f8377c259
01Uzbdeveloper <64874244+01Uzbdeveloper@users.noreply.github.com>
2020-05-22 15:57:45 +0500

    Create subject.php

---
+++ subject.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+    <form action="index.php?p=englishtest-form" method="post">
+    <h1 style="text-align:center; font-size:50px;color:white">WELCOME TO  QUIZ BOX </h1>
+    <h1 style="font-size:40px;color:black ">More than just a test </h1>
+    <p style="font-size:30px;color:black">-Supporting high-quality test materials</p>
+    <p style="font-size:30px;color:black">-Improving gramatical knowledge of English</p>
+    <p style="font-size:30px;color:black">-Preparing students for university exams</p>           
+<p><em>This site will provide the quiz for various subjects. </em></p>
+ <label for="subjects">Choose a subject:</label>
+<label style="color: black"></label>
+<select id="subjects">
+
+<option value="En">English</option>
+
+</select>
+<input type="submit" value="Submit"> 
+<br>
+ 
+ 
+<a href="index.php?action=logout"><ins>Logout</ins></a> 
+
+</form>
+    </body>
+</html>
+<?php
